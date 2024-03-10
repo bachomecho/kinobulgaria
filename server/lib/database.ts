ffd4cb5f76c862b412a1476c1106c9277e3fa1ff
Ilia Mechkarov <64164772+bachomecho@users.noreply.github.com>
2024-03-10 23:14:05 +0200

    updated movies

--- server/lib/database.ts
+++ server/lib/database.ts
@@ -1,52 +1,67 @@
 export const movies = [
   {
-    title: 'Момчето си отива',
-    thumbnail_name: 'momcheto_image',
-    video_id: '_uuhDvvY1_Q'
+    title: "Момчето си отива",
+    thumbnail_name: "momcheto_image",
+    video_id: "_uuhDvvY1_Q",
   },
   {
-    title: 'Господин за един ден',
-    thumbnail_name: 'gospodin_image',
-    video_id: 'evqlOa5youA'
+    title: "Господин за един ден",
+    thumbnail_name: "gospodin_image",
+    video_id: "evqlOa5youA",
   },
   {
-    title: 'Време разделно',
-    thumbnail_name: 'vreme_razdelno_image',
-    video_id: 'UCWF9ZJ-ibY'
+    title: "Време разделно",
+    thumbnail_name: "vreme_razdelno_image",
+    video_id: "UCWF9ZJ-ibY",
   },
   {
-    title: 'Козият рог',
-    thumbnail_name: 'kozijat_rog_image',
-    video_id: 'kHE2qQuLHy4'
+    title: "Козият рог",
+    thumbnail_name: "kozijat_rog_image",
+    video_id: "kHE2qQuLHy4",
   },
   {
-    title: 'Да обичаш на инат',
-    thumbnail_name: 'inat_image',
-    video_id: '1PHqIndDyTI'
+    title: "Да обичаш на инат",
+    thumbnail_name: "inat_image",
+    video_id: "1PHqIndDyTI",
   },
   {
-    title: 'Крадецът на праскови',
-    thumbnail_name: 'kradec_image',
-    video_id: '7kwmpkXH2Nc'
+    title: "Крадецът на праскови",
+    thumbnail_name: "kradec_image",
+    video_id: "7kwmpkXH2Nc",
   },
   {
-    title: 'Двойникът',
-    thumbnail_name: 'dvoinik_image',
-    video_id: '1N7wAD5UkKQ'
+    title: "Двойникът",
+    thumbnail_name: "dvoinik_image",
+    video_id: "1N7wAD5UkKQ",
   },
   {
-    title: 'Мъжки времена',
-    thumbnail_name: 'mujki_image',
-    video_id: 'ITwS-bhDJr4'
+    title: "Любимец 13",
+    thumbnail_name: "lubimec_image",
+    video_id: "aSOp9XhySyM",
   },
   {
-    title: 'Любимец 13',
-    thumbnail_name: 'lubimec_image',
-    video_id: 'aSOp9XhySyM'
+    title: "Баш майсторът",
+    thumbnail_name: "maistor_image",
+    video_id: "qbe3WrXl5sM",
   },
   {
-    title: 'Баш майсторът',
-    thumbnail_name: 'maistor_image',
-    video_id: 'qbe3WrXl5sM'
-  }
-]
+    title: "Свобода или смърт",
+    thumbnail_name: "svoboda_smart_image",
+    video_id: "v2c--Ir1KNc",
+  },
+  {
+    title: "С деца на море",
+    thumbnail_name: "deca_na_more_image",
+    video_id: "AqK_BACqmjo",
+  },
+  {
+    title: "Куче в чекмедже",
+    thumbnail_name: "kuche_image",
+    video_id: "UyKmIsS8bTU",
+  },
+  {
+    title: "Хитър Петър",
+    thumbnail_name: "hitur_petur_image",
+    video_id: "rzS1OqsB3gw",
+  },
+];
