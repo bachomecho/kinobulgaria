37d1be78c0038125d51f8c4e830bb1e6d7781cac
Ilia Mechkarov <64164772+bachomecho@users.noreply.github.com>
2024-05-14 20:43:34 +0300

    feat: searching functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import Home from "./components/Home";
 import About from "./components/About";
 
 export default function App() {
-  return (
-    <div className="wrapper">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
+	return (
+		<div className="wrapper">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/about" element={<About />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,21 +1,10 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import MovieItem from "./MovieItem";
 import Header from "./Header";
 
 export default function Home() {
 	const [movies, setMovies] = useState<Movie[]>([]);
 	const [search, setSearch] = useState<string>("");
-	const [selectedMovie, setSelectedMovie] = useState<Movie>({
-		title: "",
-		thumbnail_name: "",
-		video_id: "",
-		duration: 0,
-		release_year: 0,
-		director: "",
-	});
-	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-		setSearch(e.target.value);
-	}; // TODO: adjust search functions with new Header component
 
 	async function getPageData() {
 		const response = await fetch("/api/movies");
@@ -27,7 +16,6 @@ export default function Home() {
 	useEffect(() => {
 		getPageData();
 	}, []);
-	useEffect(() => console.log(selectedMovie), [selectedMovie]); // TODO: remove this
 
 	function filterMovies(): Movie[] {
 		if (search === "") {
@@ -42,7 +30,7 @@ export default function Home() {
 	const filteredMovies = filterMovies();
 	return (
 		<>
-			<Header />
+			<Header setSearchQuery={setSearch} />
 			<main className="container mx-auto py-12 px-4 md:px-6 lg:px-8">
 				<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
 					{filteredMovies.length > 0 ? (
