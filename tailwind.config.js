5fb09d0092747391286531d9ceb785352db6d404
Ilia Mechkarov <64164772+bachomecho@users.noreply.github.com>
2024-10-06 17:03:14 +0300

    refactor: organize css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
 export default {
 	content: ["./src/**/*.{js,jsx,ts,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: "#3490dc",
+				"primary-foreground": "#fff",
+				ring: "#5a67d8",
+				background: "#f7fafc",
+			},
+		},
 	},
 	plugins: [],
 };
